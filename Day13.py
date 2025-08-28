5cdaf417cfc71416f7e98e71222fe1121df3e6b5
GNS <95519634+GNS-chaos@users.noreply.github.com>
2025-08-28 08:26:54 +0300

    Enhance debugging steps and input validation
    Added debugging steps and improved error handling for age input.

--- Day13.py
+++ Day13.py
@@ -1,1 +1,23 @@
+#Debugging
 
+"""Grace Hopper: bugging history
+
+1. Describe the problem
+2. Reprodıce the bug
+3. Play computer
+4. Fix the errors
+5. use print
+6. use a debugger
+7. take a break xd
+8. ask a friend
+9. ask stackoverflow
+"""
+
+try:
+  age=int(input("age=?"))
+except ValueError:
+  print("invalid num")
+  age=(int(input("age=?")
+
+  new_item=math.add(new_item, item)
+           
